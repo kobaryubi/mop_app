188918b5c67ac4e4874de7e9f2afecda91ef0e2c
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-10-27 22:36:52 +0900

    first page, second page

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+// タイマー作る
+
+class FirstPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Page')),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return SecondPage();
+              }),
+            );
+          },
+          child: const Text('Next Page'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Page')),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back'),
         ),
       ),
     );
